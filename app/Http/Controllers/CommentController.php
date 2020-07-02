8028b0997b9a298fcc4b3e4ce74724f7498b4aab
shobuj718 <shobujsa93@gmail.com>
2020-07-02 21:10:07 +0600

    comment system simple app

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Comment;
+
+class CommentController extends Controller
+{
+    public function store(Request $request)
+    {
+    	//dd('call');
+    	$request->validate([
+            'body'=>'required',
+        ]);
+   
+        $input = $request->all();
+        $input['user_id'] = auth()->user()->id;
+    
+        Comment::create($input);
+   
+        return back();
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/posts', 'PostController@index')->name('posts.index');
+Route::get('/create', 'PostController@create')->name('posts.create');
+Route::post('/store', 'PostController@store')->name('posts.store');
+Route::get('/show/{id}', 'PostController@show')->name('posts.show');
+
+Route::post('/comments', 'CommentController@store')->name('comments.store');
